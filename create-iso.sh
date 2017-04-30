f849c73ba4e5b75185f04921c6f04e5436f9fa50
Martin Unzner <martin.u@posteo.de>
2017-05-01 00:38:54 +0200

    Check if working directory already exists

--- create-iso.sh
+++ create-iso.sh
@@ -3,7 +3,9 @@ set -o errexit
 set -o nounset
 
 cd "$(dirname "$0")"
-unzip clonezilla-live-2.5.0-25-i686.zip -d clonezilla-iso
+if [[ ! -d clonezilla-iso ]]; then
+    unzip clonezilla-live-2.5.0-25-i686.zip -d clonezilla-iso
+fi
 cp syslinux/isolinux.cfg clonezilla-iso/syslinux/isolinux.cfg
 cd clonezilla-iso
 mkisofs -A 'Clonezilla live CD' -f -r -hide-joliet-trans-tbl -J -l -b syslinux/isolinux.bin -c syslinux/boot.cat -no-emul-boot -boot-load-size 4 -boot-info-table -o ../clonezilla.iso ./
